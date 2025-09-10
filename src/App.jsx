9277ae215010b2db4120f464a6b9a2231fe747a0
Iqhuan Hariirie <iqhuanhariirie1173@gmail.com>
2025-09-10 17:52:03 +0800

    create home component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Home from './components/Home'
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
-
     </>
 
   );
