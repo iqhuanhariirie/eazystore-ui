ad2b49aa511fd9f917186218a5d14bfde2770e4b
Iqhuan Hariirie <iqhuanhariirie1173@gmail.com>
2025-09-10 17:38:37 +0800

    create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <p>&copy; 2025 <a href="https://eazystickerz.com" target="_blank" rel="noopener noreferrer">EazyStickerz</a>. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
