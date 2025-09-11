1667fe7633b6067f3595136a413b5a6224345930
Iqhuan Hariirie <iqhuanhariirie1173@gmail.com>
2025-09-11 13:20:39 +0800

    create page title component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,10 @@ import PageHeading from './PageHeading'
 export default function Home() {
     return (
         <div className="home-container">
-            <PageHeading />
+            <PageHeading title="Explore Eazy Stickerz">
+                Discover a wide range of stickers for all your needs. From custom designs to official merchandise, we have you covered.
+            </PageHeading>
+
         </div>
     );
 }
--- src/components/PageHeading.jsx
+++ src/components/PageHeading.jsx
@@ -1,7 +1,12 @@
-export default function PageHeading() {
+import PageTitle from './PageTitle';
+export default function PageHeading({title, children}) {
     return (
         <div className="page-heading-container">
-            <h1 className="page-title">Page Heading</h1>
+            <PageTitle title={title} />
+            <p className="page-heading-paragraph">
+                {children}
+            </p>
         </div>
+        
     );
 }
