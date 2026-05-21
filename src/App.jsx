ca9ee15e6061222185bd03637e10f634023454f0
iqhuanhariirie <iqhuanhariirie1173@gmail.com>
2026-05-21 17:38:00 +0800

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import AddProduct from './pages/AddProduct'
+import EditProduct from './pages/EditProduct'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/add" element={<AddProduct />} />
+        <Route path="/products/edit/:id" element={<EditProduct />} />
       </Routes>
       <Footer />
     </BrowserRouter>
